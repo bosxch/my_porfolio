d77688ce9b27ea66676179db17158d913543b97e
bosxch <amparocardosobosch@gmail.com>
2023-03-15 13:28:46 -0300

    acomplishments component

--- src/components/Acomplishments/Acomplishments.js
+++ src/components/Acomplishments/Acomplishments.js
@@ -11,9 +11,17 @@ const data = [
 ];
 
 const Acomplishments = () => (
-  <div>
-    Acomplishments
-  </div>
+ <Section>
+  <SectionTitle>Personal Acomplishments</SectionTitle>
+  <Boxes>
+    {data.map((card, idx) => {
+      <Box key={idx}>
+        <BoxNum>{card.number}+</BoxNum>
+        <BoxText>{card.text}</BoxText>
+      </Box>
+    })}
+  </Boxes>
+ </Section>
 );
 
 export default Acomplishments;
