2f24b78e3d001cf11c7258ba7f601f6053d9a363
bosxch <amparocardosobosch@gmail.com>
2023-03-19 23:02:02 -0300

    last changes in Heros styles

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -4,7 +4,7 @@ import { BlogCard, CardInfo, ExternalLinks, GridContainer, HeaderThree, Hr, Tag,
 import { Section, SectionDivider, SectionTitle } from '../../styles/GlobalComponents';
 import { projects } from '../../constants/constants';
 import { Modal, Box } from "@mui/material";
-import {ProjectCard} from '../ProjectCard/ProjectCard'
+import {ProjectCard} from '../ProjectCard/ProjectCard.jsx'
 import {useState} from 'react'
 
 const Projects = () => {
